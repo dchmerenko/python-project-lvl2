9632c6ac097f8459b99a31db6fd97e4c314be64b
Dmitry Chmerenko <dchmerenko@gmail.com>
2021-06-01 13:56:36 +0300

    Refactor generate_diff to return string

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -2,32 +2,45 @@
 
 import json
 
+ADDED = '+ '
+UNCHANGED = '  '
+REMOVED = '- '
+INDENT = '  '
+ROW_TEMPLATE = '{0}: {1}'
+
 
 def generate_diff(file_path1, file_path2):
+    """Return file difference.
 
-    diff = {}
+    Args:
+        file_path1: path to first file
+        file_path2: path to second file
 
-    ADDED = '+ '
-    UNCHANGED = '  '
-    REMOVED = '- '
+    Returns:
+        multi-string with differences
+    """
+    diff = []
 
-    json1 = json.load(open(file_path1))
-    json2 = json.load(open(file_path2))
+    with open(file_path1) as f1:
+        json1 = json.load(f1)
+    with open(file_path2) as f2:
+        json2 = json.load(f2)
 
     added_keys = json2.keys() - json1.keys()
-    unchanged_keys = json1.keys() & json2.keys()
     removed_keys = json1.keys() - json2.keys()
 
     for key in sorted(json1.keys() | json2.keys()):
-        if key in unchanged_keys:
-            if json1[key] == json2[key]:
-                diff[UNCHANGED + key] = json1[key]
-            else:
-                diff[REMOVED + key] = json1[key]
-                diff[ADDED + key] = json2[key]
-        elif key in removed_keys:
-            diff[REMOVED + key] = json1[key]
+        if key in removed_keys:
+            diff.append(ROW_TEMPLATE.format(REMOVED + key, json1[key]))
         elif key in added_keys:
-            diff[ADDED + key] = json2[key]
+            diff.append(ROW_TEMPLATE.format(ADDED + key, json2[key]))
+        else:  # key is unchanged
+            if json1[key] == json2[key]:
+                diff.append(ROW_TEMPLATE.format(UNCHANGED + key, json1[key]))
+            else:
+                diff.append(ROW_TEMPLATE.format(REMOVED + key, json1[key]))
+                diff.append(ROW_TEMPLATE.format(ADDED + key, json2[key]))
 
-    return diff
+    rows = '\n{0}'.format(INDENT).join(diff)
+
+    return ''.join(('{\n', INDENT, rows, '\n}'))
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """Difference Calculator main script."""
 
-import argparse, pprint
+import argparse
 
 from gendiff import generate_diff
 
@@ -11,22 +11,20 @@ def main():
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f', '--format',
-                        # choices=['plain', 'json'],
-                        default='plain',
-                        help='set format of output')
+    parser.add_argument(
+        '-f',
+        '--format',
+        choices=['plain', 'json'],
+        default='plain',
+        help='set format of output',
+    )
     args = parser.parse_args()
-
-    print('first file:', args.first_file)
-    print('second file:', args.second_file)
-    print('format:', args.format)
 
     file_path1 = args.first_file
     file_path2 = args.second_file
-    format_ = args.format
 
     diff = generate_diff(file_path1, file_path2)
-    pprint.pprint(diff)
+    print(diff)
 
 
 if __name__ == '__main__':
