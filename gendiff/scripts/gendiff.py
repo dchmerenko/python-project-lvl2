b0b7f4126120e62272781027f7c0565d8c1c6fbe
Dmitry Chmerenko <dchmerenko@gmail.com>
2021-06-01 10:33:08 +0300

    Step 2 is finished

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -9,7 +9,12 @@ def main():
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', '--format',
+                        # choices=['plain', 'json'],
+                        default='plain',
+                        help='set format of output')
     parser.parse_args()
+
 
 
 if __name__ == '__main__':
