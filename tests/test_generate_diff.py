07156ac5453bccf09fc0f28e39b560ffe8449914
Dmitry Chmerenko <dchmerenko@gmail.com>
2021-06-03 14:17:47 +0300

    Update test test_scripts_gendiff.py
    Add test test_scripts_plain_json

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -4,11 +4,13 @@ from gendiff.gendiff import generate_diff
 
 @pytest.fixture
 def plain_json_diff():
-    with open('tests/fixtures/file1.json_file2.json_diff') as f:
-        return f.read()
+    """Return plain json files diff example."""
+    return open('tests/fixtures/file1.json_file2.json_diff').read()
 
 
 def test_generate_diff(plain_json_diff):
-    diff = generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json')
-
-    assert diff == plain_json_diff
+    """Test plain json files differences."""
+    assert plain_json_diff == generate_diff(
+        'tests/fixtures/file1.json',
+        'tests/fixtures/file2.json',
+    )
--- tests/test_scripts_gendiff.py
+++ tests/test_scripts_gendiff.py
@@ -1,11 +1,24 @@
 import pytest
 import subprocess
 
+from test_generate_diff import plain_json_diff
+
 
 @pytest.fixture
-def gendiff_h_out():
-    return open("tests/fixtures/gendiff_h_out").read()
+def gendiff_help_message():
+    """Return script help message."""
+    return open("tests/fixtures/gendiff_help_message").read()
 
 
-def test_scripts_gendiff_h(gendiff_h_out):
-    assert gendiff_h_out == subprocess.check_output(["gendiff", "-h"], universal_newlines=True)
+def test_scripts_gendiff_help_message(gendiff_help_message):
+    assert gendiff_help_message == subprocess.check_output(
+        ["gendiff", "-h"],
+        universal_newlines=True,
+    )
+
+
+def test_scripts_plain_json(plain_json_diff):
+    assert plain_json_diff == subprocess.check_output(
+        ["gendiff", "tests/fixtures/file1.json", "tests/fixtures/file2.json"],
+        universal_newlines=True,
+    )[:-1]  # delete last '\n'
