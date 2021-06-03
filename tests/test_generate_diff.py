0b22f562a102f78135a411f4738faeb908685615
Dmitry Chmerenko <dchmerenko@gmail.com>
2021-06-03 07:27:41 +0300

    Add pytest.fixture
    Delete test_example.py
    Move file1.json, file2.json to tests/fixtures
    Create file1.json_file2.json_diff
    Add pytest.fixture plain_json_diff

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,14 +1,14 @@
+import pytest
 from gendiff.gendiff import generate_diff
 
 
-def test_generate_diff():
-    diff = generate_diff('tests/file1.json', 'tests/file2.json')
-    result = """{
-  - follow: False
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: True
-}"""
-    assert result == diff
+@pytest.fixture
+def plain_json_diff():
+    with open('tests/fixtures/file1.json_file2.json_diff') as f:
+        return f.read()
+
+
+def test_generate_diff(plain_json_diff):
+    diff = generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json')
+
+    assert diff == plain_json_diff
