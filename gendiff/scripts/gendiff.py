090deba70fb2ae4376c020cf1e6954bd75d72953
Dmitry Chmerenko <dchmerenko@gmail.com>
2021-06-26 14:20:58 +0300

    Refactor: Add get_arg function

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,22 +6,39 @@ import argparse
 from gendiff.gendiff import generate_diff
 
 
-def main():
-    """Calculate file difference."""
-    parser = argparse.ArgumentParser(description='Generate diff')
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
+def get_args():
+    """Get command line arguments.
+
+    Returns:
+        command line args
+    """
+    parser = argparse.ArgumentParser(
+        description='Compares two text files and shows a difference.',
+        usage='%(prog)s [options] <filepath1> <filepath2>',  # noqa:WPS323
+    )
+
+    parser.add_argument('first_file', help=argparse.SUPPRESS)
+    parser.add_argument('second_file', help=argparse.SUPPRESS)
     parser.add_argument(
         '-f',
         '--format',
+        dest='format_name',
+        metavar='',
+        choices=['json', 'plain', 'stylish'],
         default='stylish',
-        help='set format of output',
+        help='output format [json | plain | stylish] (default: stylish)',
     )
-    args = parser.parse_args()
+
+    return parser.parse_args()
+
+
+def main():
+    """Calculate file difference."""
+    args = get_args()
 
     file_path1 = args.first_file
     file_path2 = args.second_file
-    format_name = args.format
+    format_name = args.format_name
 
     diff = generate_diff(file_path1, file_path2, format_name=format_name)
 
