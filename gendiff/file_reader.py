e06e25ce6f7e3568a7aaa2d306659650de0fac18
Dmitry Chmerenko <dchmerenko@gmail.com>
2022-02-22 23:21:59 +0300

    Refactor data_reader.py, data_parser.py
    Rename data_reader.py to file_reder.py
    Move formats to data_parser.py
    Fix naming

---
+++ gendiff/file_reader.py
@@ -0,0 +1,20 @@
+"""Read data from a file."""
+import os
+
+
+def read_file(file_path):
+    """Read file content.
+
+    Args:
+        file_path: path to file
+
+    Returns:
+        file content, file extension
+    """
+    _, extension = os.path.splitext(file_path)
+    extension = extension.lower()
+
+    with open(file_path) as data_file:
+        content = data_file.read()
+
+    return content, extension
